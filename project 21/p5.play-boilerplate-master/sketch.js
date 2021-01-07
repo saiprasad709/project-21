b2674a760dfc78307bd8b8ea10dac16b7a64c46e
saiprasad709 <75840696+saiprasad709@users.noreply.github.com>
2021-01-07 10:54:55 +0530

    Add files via upload

---
+++ project 21/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,54 @@
+ var bullet,wall,thickness;
+var speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400,);
+  
+  speed=random(223,321);
+   weight=random(30,52);
+    thickness=random(22,83);
+
+  bullet=createSprite(50, 200, 50, 10);
+    bullet.velocityX=speed;
+     bullet.shapecolor=color(255);
+
+  wall=createSprite(1200,200,thickness,height/2);
+    wall.shapecolor=color(80,80,80);
+
+}
+
+function draw() {
+   background(0,0,0);
+
+ if(hascollided (bullet,wall)){
+   bullet.velocityX=0 ;
+   var damage=0.5  * weight * speed * speed/(thickness * thickness * thickness)
+ 
+   if( damage > 10 ){
+     wall.shapecolor=color(255,0,0);
+   }
+   if( damage < 10 ){
+     wall.shapecolor=color(0,255,0);
+   }
+ }
+   
+  drawSprites();
+}
+
+function hascollided(bullet,wall) {
+
+bulletRightEdge=bullet.x + bullet.width;
+
+wallLeftEdge=wall.x ;
+
+if(bulletRightEdge>=wallLeftEdge){
+
+  
+  return true ;
+
+}
+
+  return false ;
+
+}
